3f5189ca117f4ec437639934edd5d8c309ff058c
wisild <matveychocolate2000@gmail.com>
2026-05-15 18:27:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,120 +1,79 @@
-import telebot
-
-    # This bot is needed to connect two people and their subsequent anonymous communication
-#
-# Avaiable commands:
-# `/start` - Just send you a messsage how to start
-# `/find` - Find a person you can contact
-# `/stop` - Stop active conversation
+import os
 
 import telebot
-from telebot import types
 
-# Initialize bot with your token
-bot = telebot.TeleBot('TOKEN')
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
 
-# The `users` variable is needed to contain chat ids that are either in the search or in the active dialog, like {chat_id, chat_id}
-users = {}
-# The `freeid` variable is needed to contain chat id, that want to start conversation
-# Or, in other words: chat id of user in the search
-freeid = None
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
 
-# `/start` command handler
-#
-# That command only sends you 'Just use /find command!'
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
+
+if "TELEBOT_BOT_TOKEN" not in os.environ or "GROUP_CHAT_ID" not in os.environ:
+    raise AssertionError("Please configure TELEBOT_BOT_TOKEN and GROUP_CHAT_ID as environment variables")
+
+bot = telebot.AsyncTeleBot(os.environ["TELEBOT_BOT_TOKEN"])
+GROUP_CHAT_ID = int(os.environ["GROUP_CHAT_ID"])
+
+
+def is_api_group(chat_id):
+    return chat_id == GROUP_CHAT_ID
+
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
+
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, text_messages['welcome'].format(name=name))
+
+
+@bot.message_handler(commands=['info', 'help'])
+def on_info(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+    bot.reply_to(message, text_messages['info'])
+
+
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+
 @bot.message_handler(commands=['start'])
-def start(message: types.Message):
-    bot.send_message(message.chat.id, 'Just use /find command!')
+def on_start(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
 
-# `/find` command handler
-#
-# That command finds opponent for you
-#
-# That command according to the following principle:
-# 1. You have written `/find` command
-# 2. If you are already in the search or have an active dialog, bot sends you 'Shut up!'
-# 3. If not:
-#   3.1. Bot sends you 'Finding...'
-#   3.2. If there is no user in the search:
-#       3.2.2. `freeid` updated with `your_chat_id`
-#   3.3. If there is user in the search:
-#       3.3.1. Both you and the user in the search recieve the message 'Founded!'
-#       3.3.2. `users` updated with a {user_in_the_search_chat_id, your_chat_id}
-#       3.3.3. `users` updated with a {your_chat_id, user_in_the_search_id}
-#       3.3.4. `freeid` updated with `None`
-@bot.message_handler(commands=['find'])
-def find(message: types.Message):
-    global freeid
 
-    if message.chat.id not in users:
-        bot.send_message(message.chat.id, 'Finding...')
+def listener(messages):
+    for m in messages:
+        print(str(m))
 
-        if freeid is None:
-            freeid = message.chat.id
-        else:
-            # Question:
-            # Is there any way to simplify this like `bot.send_message([message.chat.id, freeid], 'Founded!')`?
-            bot.send_message(message.chat.id, 'Founded!')
-            bot.send_message(freeid, 'Founded!')
 
-            users[freeid] = message.chat.id
-            users[message.chat.id] = freeid
-            freeid = None
-
-        print(users, freeid) # Debug purpose, you can remove that line
-    else:
-        bot.send_message(message.chat.id, 'Shut up!')
-
-# `/stop` command handler
-#
-# That command stops your current conversation (if it exist)
-#
-# That command according to the following principle:
-# 1. You have written `/stop` command
-# 2. If you are not have active dialog or you are not in search, bot sends you 'You are not in search!'
-# 3. If you are in active dialog:
-#   3.1. Bot sends you 'Stopping...'
-#   3.2. Bot sends 'Your opponent is leavin`...' to your opponent
-#   3.3. {your_opponent_chat_id, your_chat_id} removes from `users`
-#   3.4. {your_chat_id, your_opponent_chat_id} removes from `users`
-# 4. If you are only in search:
-#   4.1. Bot sends you 'Stopping...'
-#   4.2. `freeid` updated with `None`
-@bot.message_handler(commands=['stop'])
-def stop(message: types.Message):
-    global freeid
-
-    if message.chat.id in users:
-        bot.send_message(message.chat.id, 'Stopping...')
-        bot.send_message(users[message.chat.id], 'Your opponent is leavin`...')
-
-        del users[users[message.chat.id]]
-        del users[message.chat.id]
-        
-        print(users, freeid) # Debug purpose, you can remove that line
-    elif message.chat.id == freeid:
-        bot.send_message(message.chat.id, 'Stopping...')
-        freeid = None
-
-        print(users, freeid) # Debug purpose, you can remove that line
-    else:
-        bot.send_message(message.chat.id, 'You are not in search!')
-
-# message handler for conversation
-#
-# That handler needed to send message from one opponent to another
-# If you are not in `users`, you will recieve a message 'No one can hear you...'
-# Otherwise all your messages are sent to your opponent
-#
-# Questions:
-# 1. Is there any way to improve readability like `content_types=['all']`?
-# 2. Is there any way to register this message handler only when i found the opponent?
-@bot.message_handler(content_types=['animation', 'audio', 'contact', 'dice', 'document', 'location', 'photo', 'poll', 'sticker', 'text', 'venue', 'video', 'video_note', 'voice'])
-def chatting(message: types.Message):
-    if message.chat.id in users:
-        bot.copy_message(users[message.chat.id], users[users[message.chat.id]], message.id)
-    else:
-        bot.send_message(message.chat.id, 'No one can hear you...')
-
-bot.infinity_polling(skip_pending=True)
-bot.polling()
+bot.set_update_listener(listener)
+bot.infinity_polling()
